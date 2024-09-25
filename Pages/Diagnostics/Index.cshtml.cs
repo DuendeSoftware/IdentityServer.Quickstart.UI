c8f4e2c22c43c4c253dac09c8d0dcf25c1cb5440
Roland Guijt <roland.guijt@gmail.com>
2024-09-25 14:47:12 +0200

    Secure ServerSideSessions page in the same way as Diagnostics

--- Pages/Diagnostics/Index.cshtml.cs
+++ Pages/Diagnostics/Index.cshtml.cs
@@ -16,16 +16,9 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new List<string?> { "127.0.0.1", "::1" };
-        if(HttpContext.Connection.LocalIpAddress != null)
-        {
-            localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
-        }
-
-        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
-        {
+        //Replace with an authorization policy check
+        if (HttpContext.Connection.IsRemote())
             return NotFound();
-        }
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
             
