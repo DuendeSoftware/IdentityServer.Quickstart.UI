8b2d496b7477ec2783f38d1d75229dcc48476138
Maarten Balliauw <maarten@maartenballiauw.be>
2025-02-17 13:08:19 +0100

    Update namespace for IdentityModel to Duende.IdentityModel

--- Pages/Consent/Index.cshtml.cs
+++ Pages/Consent/Index.cshtml.cs
@@ -1,12 +1,12 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
+using Duende.IdentityModel;
 using Duende.IdentityServer.Events;
 using Duende.IdentityServer.Extensions;
 using Duende.IdentityServer.Models;
 using Duende.IdentityServer.Services;
 using Duende.IdentityServer.Validation;
-using IdentityModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
